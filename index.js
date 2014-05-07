8594a9dc813af7f46d700d04b72cb9f8d6bdadd2
Zach Schneider <schneidmaster@gmail.com>
2014-05-07 17:14:26 -0400

    Updates
    + Added dependency management (npm, bower, grunt)
    + Fixed issue with access token not stored in session
    + Fixed issue with unnecessary access token requests

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ handle["/"] = requestHandlers.welcome;
 handle["/welcome"] = requestHandlers.welcome;
 handle["/welcome.html"] = requestHandlers.welcome;
 handle["/start"] = requestHandlers.start;
-handle["/assets/js/clock.js"] = requestHandlers.loadJs;
-handle["/assets/css/flipclock.css"] = requestHandlers.loadCss;
-handle["/assets/js/flipclock.min.js"] = requestHandlers.loadJs;
-handle["/assets/js/libs/jquery.js"] = requestHandlers.loadJs;
 handle["/flow.html"] = requestHandlers.flow;
 handle["/flow"] = requestHandlers.flow;
-handle["/assets/js/flow.js"] = requestHandlers.loadJs;
-handle['/assets/css/bootstrap.css'] = requestHandlers.loadCss;
-handle['/assets/css/flow.css'] = requestHandlers.loadCss;
-handle['/assets/js/bootstrap.min.js'] = requestHandlers.loadJs;
+handle["/sheet"] = requestHandlers.sheet;
 handle['/tweet'] = requestHandlers.tweet;
+
+handle["/assets/js/clock.js"] = requestHandlers.loadJs;
+handle["/assets/js/flow.js"] = requestHandlers.loadJs;
+handle['/assets/css/flow.css'] = requestHandlers.loadCss;
+
+handle['/assets/css/lib.css'] = requestHandlers.loadCss;
+handle['/assets/js/lib.js'] = requestHandlers.loadJs;
 
 server.start(router.route, handle);
--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,6 @@
 var querystring = require("querystring");
 var fs = require("fs");
+var path = require('path');
 
 function welcome(response, postData, pathname, request) {
 	loadStatic(response, postData, "/welcome.html");
@@ -33,26 +34,41 @@ function flow(response, postData, pathname, request, getData) {
 	var twitterAPI = require('node-twitter-api');
 	var connect = require('connect');
 	var sess = request.session;
-	//oauth_verifier is stored in GET, requestToken and requestTokenSecret are stored in session
-	requestToken = sess.requestToken;
-	requestTokenSecret = sess.requestTokenSecret;
-	oauth_verifier = getData.oauth_verifier
-	console.log("oauth_verifier: " + oauth_verifier);
-	var twitter = new twitterAPI({
-		consumerKey: 'fSORn4SdW6ZLSGHkT8baa6VlT',
-		consumerSecret: 'pv8s43M2eL5tkcmwcKJVMeB5uq9nszVCSH8h343TBuwGbEq8W0',
-		callback: 'http://54.219.148.68/flow'
-	});
-	twitter.getAccessToken(requestToken, requestTokenSecret, oauth_verifier, function(error, accessToken, accessTokenSecret, results) {
-		if (error) {
-			console.log(error);
-		} else {
-			//store accessToken and accessTokenSecret in session
-			sess.accessToken = accessToken;
-			sess.accessTokenSecret = accessTokenSecret;			
-		}
-	});
-	loadStatic(response, postData, "/flow.html");
+
+	// Redirect if we already have an access token
+	if(sess.accessToken != null) {
+		response.writeHead(302, {"Location": "/sheet" });
+		response.end();
+	}
+
+	else {
+		//oauth_verifier is stored in GET, requestToken and requestTokenSecret are stored in session
+		requestToken = sess.requestToken;
+		requestTokenSecret = sess.requestTokenSecret;
+		oauth_verifier = getData.oauth_verifier
+		console.log("oauth_verifier: " + oauth_verifier);
+		var twitter = new twitterAPI({
+			consumerKey: 'fSORn4SdW6ZLSGHkT8baa6VlT',
+			consumerSecret: 'pv8s43M2eL5tkcmwcKJVMeB5uq9nszVCSH8h343TBuwGbEq8W0',
+			callback: 'http://54.219.148.68/flow'
+		});
+		twitter.getAccessToken(requestToken, requestTokenSecret, oauth_verifier, function(error, accessToken, accessTokenSecret, results) {
+			if (error) {
+				console.log(error);
+			} else {
+				//store accessToken and accessTokenSecret in session
+				sess.accessToken = accessToken;
+				sess.accessTokenSecret = accessTokenSecret;
+
+				response.writeHead(302, {"Location": "/sheet" });
+				response.end();
+			}
+		});
+	}
+}
+
+function sheet(response, postData, pathname, request) {
+	loadStatic(response, postData, "/sheet.html");
 }
 
 function tweet(response, postData, pathname, request) {
@@ -68,6 +84,7 @@ function tweet(response, postData, pathname, request) {
 	var sess = request.session;
 	sess.twitter++;
 	sess.lastTweet = tweet;
+
 	accessToken = sess.accessToken;
 	accessTokenSecret = sess.accessTokenSecret;
 	twitter.statuses("update", {
@@ -89,7 +106,7 @@ function tweet(response, postData, pathname, request) {
 }
 
 function loadStatic(response, postData, pathname) {
-	path = "." + pathname;
+	path = "./www" + pathname;
 	fs.readFile(path, function (err, data) {
 		if (err) throw err;
 		//console.log("static HTML page " + pathname + " called");
@@ -126,6 +143,7 @@ exports.loadJs = loadJs;
 exports.loadStatic = loadStatic;
 exports.loadCss = loadCss;
 exports.flow = flow;
+exports.sheet = sheet;
 exports.tweet = tweet;
 exports.welcome = welcome;
 
